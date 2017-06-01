154e7d5f6b035b85827cb38b9a7eec83e92c352c
hliyuan <598059394@qq.com>
2017-06-01 11:11:01 +0800

    commit main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
+  var emailArray = email.split("@");
+  if (emailArray[1]==suffixes[0]) {
+  	return true;
+  }else{
+  	return false;
+  }
 };
